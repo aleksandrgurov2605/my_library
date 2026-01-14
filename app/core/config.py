30ef91c5fd145d7ecdcd648b2127ae435859a055
Aleksandr Gurov <tevez89_89@mail.ru>
2026-01-14 16:00:35 +0700

    edit config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,22 +5,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MODE: Literal['DEV', 'TEST', 'PROD', 'LITE']
+    MODE: Literal['DEV', 'TEST', 'PROD']
 
     POSTGRES_DB: str
     POSTGRES_PASSWORD: str
     POSTGRES_USER: str
     POSTGRES_HOST: str
     POSTGRES_PORT: str
-
-    TEST_POSTGRES_DB: str
-    TEST_POSTGRES_PASSWORD: str
-    TEST_POSTGRES_USER: str
-    TEST_POSTGRES_HOST: str
-    TEST_POSTGRES_PORT: str
-
-    SECRET_KEY: str
-    ALGORITHM: str
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), '..', '..', '.env'),
@@ -31,11 +22,6 @@ class Settings(BaseSettings):
     def ASYNC_DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}" \
                f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-
-    @property
-    def TEST_ASYNC_DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_POSTGRES_USER}:{self.TEST_POSTGRES_PASSWORD}" \
-               f"@{self.TEST_POSTGRES_HOST}:{self.TEST_POSTGRES_PORT}/{self.TEST_POSTGRES_DB}"
 
 
 settings = Settings()
